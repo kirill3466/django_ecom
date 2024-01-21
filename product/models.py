ec2043e03143e1d8c4a3d65be83a74118d545091
greyje <petrov3466@mail.ru>
2024-01-21 13:03:17 +0500

    add img field, img paths to templates

--- product/models.py
+++ product/models.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+
+from django.core.files import File
 from django.db import models
+from PIL import Image
 
 
 class Category(models.Model):
@@ -23,6 +27,9 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/',
+                                  blank=True, null=True)
 
     class Meta:
         ordering = ('-created_at',)
@@ -32,3 +39,26 @@ class Product(models.Model):
 
     def get_display_price(self):
         return self.price / 100
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image and self.image.name:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x240.jpg'
+
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
