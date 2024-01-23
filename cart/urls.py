6ac7f9b0d1f36ae9409153bf495ba37ad43f090b
greyje <petrov3466@mail.ru>
2024-01-23 21:10:17 +0500

    add cart, checkout templates, page title

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 
 urlpatterns = [
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('', cart, name='cart'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -7,3 +7,11 @@ def add_to_cart(request, product_id):
     cart = Cart(request)
     cart.add(product_id)
     return render(request, 'cart/menu_cart.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def checkout(request):
+    return render(request, 'cart/checkout.html')
