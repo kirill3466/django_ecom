052c4b0670adca5ecb935255b39e315acf3611c4
greyje <petrov3466@mail.ru>
2024-01-24 10:13:55 +0500

    add cart functionality (decrement, increment for quantity)

--- cart/cart.py
+++ cart/cart.py
@@ -19,6 +19,7 @@ class Cart(object):
 
         for item in self.cart.values():
             item['total_price'] = int(item['product'].price * item['quantity']) / 100
+
             yield item
 
     def __len__(self):
@@ -47,10 +48,18 @@ class Cart(object):
             del self.cart[product_id]
             self.save()
 
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
+
     def get_total_cost(self):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
 
-        return int(sum(
-            item['product'].price * item['quantity'] for item in self.cart.values()
-        )) / 100
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) / 100
+
+    def get_item(self, product_id):
+        if str(product_id) in self.cart:
+            return self.cart[str(product_id)]
+        else:
+            return None
